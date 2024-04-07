474630f25b96e7e3ebce0d34af64319baf197699
kathylopez97 <146994593+kathylopez97@users.noreply.github.com>
2024-04-07 00:11:36 -0700

    adjusting to server;.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const staticPath = path.join(__dirname, '../client/dist');
